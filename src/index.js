7a39f9efc2e63877d0e0518d5e149f0f26564bd8
AndriiPetrovskiy <loulla2486@gmail.com>
2020-07-19 11:30:27 +0300

    to finish localstorage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './styles.css';
+
+// import localStorage from './template/localStorage.js';
+
+const listItemsRef = document.querySelector('.js-menu');
+// console.log(listItemsRef);
+const switchThemeRef = document.querySelector('.js-switch-input');
+// console.dir(switchTheme);
+const bodyRef = document.querySelector('body');
+console.log(bodyRef);
+
+import templatesItems from './template/templatesItems.hbs';
+import menuItems from './db/menu.json';
+
+const markup = templatesItems(menuItems);
+// console.log(markup);
+
+listItemsRef.insertAdjacentHTML('afterbegin', markup);
+
+
+switchThemeRef.addEventListener('click', switcher);
+
+function switcher () {
+    if(event.target.checked) {
+        bodyRef.classList.add('dark-theme');
+    } else {
+        bodyRef.classList.remove('dark-theme');
+    }
+}
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  localStorage.setItem('theme', JSON.stringify(Theme));
+
+console.log(JSON.stringify(Theme));
+
+
+
+
+
+
+// const bodyRef = document.querySelector('body');
+
+
+
+// const switchTheme = document.querySelector('.js-switch-input');
+
+
+// switchTheme.addEventListener('click', () => {
+//     bodyRef.classList.add('dark-theme');
+// })
+
+
+
